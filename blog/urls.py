828ef18f816b7947fa7ac8d8e745826b43c017be
uttamthummala <uttamthummala@gmail.com>
2023-12-07 10:46:48 +0530

    to support static files on render

--- blog/urls.py
+++ blog/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('articles.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
